c4bb2d50e8144a1b5ec6c698304f3db8f64dc480
henriits <121551949+henriits@users.noreply.github.com>
2023-03-11 19:46:13 +0200

    serve method and get definition start

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,21 +1,30 @@
 import justpy as jp
-
+import definition
 
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="Instant English Dictionary!", classes="text-4xl m-2")
         jp.Div(a=div, text="Get the definition of any English word instatly as you type.",
                classes="text-lg")
-        jp.Input(a=div, placeholder="Type in a word here...",
-                 classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
-                         "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
-        jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-48")
 
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                                     "focus:bg-white focus:outline-none focus:border-purple-500 py-2 px-4")
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        jp.Button(a=input_div, text="Get Definition", classes="border border-blue-500 m-3 "
+                                                              "py-1 px-4 rounded hover:bg-red-500 hover:text-white",
+                  click=cls.get_definition, outputdiv=output_div, inputbox=input_box)
         return wp
 
+    @staticmethod
+    def get_definition(widget, msg):
 
-
+        widget.outputdiv.text = widget.inputbox.value
