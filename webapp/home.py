64fab66a5a25d235bc5ddd5201d8a52b195819f1
henriits <121551949+henriits@users.noreply.github.com>
2023-03-12 17:29:10 +0200

    homepage navigation menu

--- webapp/home.py
+++ webapp/home.py
@@ -12,9 +12,18 @@ class Home:
         header = jp.QHeader(a=layout)
         toolbar = jp.QToolbar(a=header)
 
-
-
         drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="left", border=True)
+
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text="Home", href="/" ,classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="about", href="/about", classes=a_classes)
+        jp.Br(a=qlist)
+
         jp.QBtn(a=toolbar, dence=True, flat=True, round=True, icon="menu", click=cls.move_drawer, drawer=drawer)
         jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
 
